d606f5cffa0f1942f1e846e2811a917273592408
Xinyu Feng <xinyuf@usc.edu>
2025-11-13 11:35:21 -0500

    Update prference.py

--- prference.py
+++ prference.py
@@ -57,4 +57,4 @@ def main(in_tsv: str, out_jsonl: str):
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     args = ap.parse_args()
-    main("paradetox.tsv", "preference.jsonl")
+    main("para_df_with_sentiment.csv", "preference.jsonl")
